089c9e00058dc696435176f175036becbb20fb70
Carlos Parraga <carlosparraga90@hotmail.com>
2023-04-08 14:08:42 -0400

    Add some custom html and bootstrap to the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <h1>Hello, world!</h1>
+      </div>
+      <div className="col text-right">
+        Delete all tasks
+      </div>
     </div>
+
+    <div className="row mt-3">
+      <div className="col">
+        <input type="text"/>
+        <input type="submit" value="Add"/>
+      </div>
+      <div className="col">
+        <input type="text"/>
+      </div>
+    </div>
+
+    <div className="row mt-3">
+      <div className="col">
+        <h3>Pending</h3>
+        <ul>
+          <li>
+            <input type="checkbox" name="myCheckbox" value="true"/>
+              Work work
+          </li>
+          <li>
+            <input type="checkbox" name="myCheckbox" value="true"/>
+              Work work
+          </li>
+          <li>
+            <input type="checkbox" name="myCheckbox" value="true"/>
+              Work work
+          </li>
+        </ul>
+      </div>
+
+      <div className="col">
+        <h3>Completed</h3>
+        <ul>
+          <li>
+            <input type="checkbox" name="myCheckbox" value="true" checked/>
+              Work work
+          </li>
+          <li>
+            <input type="checkbox" name="myCheckbox" value="true" checked/>
+              Work work
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
   );
 }
 
